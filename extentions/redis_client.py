6dd5a4ba84960cff0051865aae8d21e543065d62
vishrutkohli <kohlivishrut@gmail.com>
2021-05-15 18:56:33 +0530

    env variables added

--- extentions/redis_client.py
+++ extentions/redis_client.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from rejson import Client
 
 from redistimeseries.client import Client as TSDBClient
-
+import os
 
 class RedisClient:
     SINGLETON_INSTANCE = None
 
     def get_instance(self):
         if not RedisClient.SINGLETON_INSTANCE:
-            RedisClient.SINGLETON_INSTANCE = Client(host='redis-12112.c245.us-east-1-3.ec2.cloud.redislabs.com',
-                                                    port=12112, decode_responses=True,
-                                                    password='HnQfZnVJtOjP7Tw7bLQgFuC9YW3RJeHK')
+            RedisClient.SINGLETON_INSTANCE = Client(host=os.environ['redis_host'],
+                                                    port=os.environ['redis_port'], decode_responses=True,
+                                                    password=os.environ['redis_password'])
 
         return RedisClient.SINGLETON_INSTANCE
 
@@ -21,8 +21,8 @@ class RedisTSClient:
 
     def get_instance(self):
         if not RedisTSClient.SINGLETON_INSTANCE:
-            RedisTSClient.SINGLETON_INSTANCE = TSDBClient(host='redis-19934.c257.us-east-1-3.ec2.cloud.redislabs.com',
-                                                    port=19934, decode_responses=True,
-                                                    password='oFtj94WehNxi2ellRF41p59gylrlF33q')
+            RedisTSClient.SINGLETON_INSTANCE = TSDBClient(host=os.environ['redis_ts_host'],
+                                                    port=os.environ['redis_ts_port'], decode_responses=True,
+                                                    password=os.environ['redis_ts_password'])
 
         return RedisTSClient.SINGLETON_INSTANCE
